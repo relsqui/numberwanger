0a7f997c632d20aae5142272416630a7ae65f2d2
Finn Ellis <finnre@cat.pdx.edu>
2015-02-13 20:37:40 -0800

    Initial commit.

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # numberwanger
 An IRC bot for playing Numberwang.
+
+# instructions
+* `cp nwbot.cfg.example nwbot.cfg`
+* edit config file as needed
+* `bash numberwanger.sh &`
+* good luck!
---
+++ numberwanger.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+log() { echo "$(date "+%F %T") $*" >> $logfile; }
+raw() { echo "$*" >> .botfile; }
+
+. nwbot.cfg
+if $has_key; then read -sp "Channel key? " chankey; fi
+if $use_ssl; then connect_command="ncat --ssl"; else connect_command="nc"; fi
+
+numberwang=$(($RANDOM & 100))
+log "starting. numberwang is $numberwang"
+
+rm .botfile 2>/dev/null
+mkfifo .botfile
+chmod 600 .botfile
+trap "rm .botfile; log 'quitting'" exit term kill
+first_run=true
+
+tail -f .botfile | $connect_command $server $port | while true; do
+    if $first_run; then
+        raw "NICK $nick"
+        raw "USER $nick 0 $nick :That's Botwang!"
+        raw "JOIN $channel $chankey"
+        first_run=false
+    fi
+    read line
+    line=$(echo "$line" | tr -d "\r\n")
+    if echo "$line" | grep -qi "^ping\b"; then
+        raw $(echo "$line" | sed "s/ping/PONG/i")
+    elif echo "$line" | grep -qi " PRIVMSG $channel :$nick: source"; then
+        raw "PRIVMSG $channel :https://github.com/relsqui/numberwanger"
+    elif echo "$line" | grep -qiv " PRIVMSG $channel :.*\b$numberwang\b"; then
+        continue
+    else
+        raw "PRIVMSG $channel :THAT'S NUMBERWANG!"
+        numberwang=$(($RANDOM & 100))
+        log "numberwang is now $numberwang"
+    fi
+done
